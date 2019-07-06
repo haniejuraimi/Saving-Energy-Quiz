f5b3134dd9ea9cc8a3c38ba230209941f5cb5212
haniejuraimi <haniealia08@gmail.com>
2019-07-06 13:35:09 +0800

    Hid alert
    I'm not done yet lmao

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
-Swal.fire(
-    'Good job!',
-    'You clicked the button!',
-    'success'
-  )
 
-  Swal.fire({
-    title: 'Custom animation with Animate.css',
-    animation: false,
-    customClass: {
-      popup: 'animated tada'
-    }
-  })
+//   Swal.fire({
+//     title: 'Custom animation with Animate.css',
+//     animation: false,
+//     customClass: {
+//       popup: 'animated tada'
+//     }
